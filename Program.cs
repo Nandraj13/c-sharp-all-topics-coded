5cea6a20ea38d0d05ae4ab34583a33e6b33dc9e1
Nandraj13 <111118850+Nandraj13@users.noreply.github.com>
2023-01-18 15:29:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class A{
+    public int age=12;
+    public void dipslay()
+    {
+        Console.WriteLine("this is class A");
+    }
+}
+class B:A{
+    static void Main(string[] args)
+    {
+        B b=new B();
+        Console.WriteLine(b.age);
+        b.dipslay();
+    }
+}
